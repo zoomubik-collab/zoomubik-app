fd234166e8af72a15fb5408fdebd3c6f9be43c4d
Zoomubik <soporte@zoomubik.com>
2026-03-17 23:02:18 +0100

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zoomubik.app',
+  appName: '\x16Zoomubik',
+  webDir: 'www'
+};
+
+export default config;
