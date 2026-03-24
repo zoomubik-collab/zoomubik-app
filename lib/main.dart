c71b2562f6f0838501078f04bc41cd7713a86a10
zoomubik-collab <zoomubik@gmail.com>
2026-03-24 20:40:24 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _controller = WebViewController()
-      ..setJavaScriptEnabled(true)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse('https://www.zoomubik.com'));
   }
 
