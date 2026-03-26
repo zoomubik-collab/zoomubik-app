925b58cae8465f3befe18f4af4338bb3a447a7f3
Zoomubik <soporte@zoomubik.com>
2026-03-27 00:42:30 +0100

    volver a flutter_inappwebview con GoogleService-Info correcto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -11,6 +12,9 @@ Future<void> main() async {
     );
   } catch (e) {
     debugPrint('Error Firebase');
+  }
+  if (!kDebugMode) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(false);
   }
   runApp(ZoomubikApp());
 }
@@ -32,23 +36,80 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  late final WebViewController _controller;
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  late InAppWebViewController _webViewController;
+  final String _homeUrl = 'https://www.zoomubik.com';
+
+  final InAppWebViewSettings _settings = InAppWebViewSettings(
+    sharedCookiesEnabled: true,
+    incognito: false,
+    thirdPartyCookiesEnabled: true,
+    databaseEnabled: true,
+    domStorageEnabled: true,
+    cacheEnabled: true,
+    cacheMode: CacheMode.LOAD_CACHE_ELSE_NETWORK,
+    useShouldOverrideUrlLoading: true,
+    mediaPlaybackRequiresUserGesture: false,
+    useHybridComposition: true,
+    allowsInlineMediaPlayback: true,
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+  );
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1')
-      ..loadRequest(Uri.parse('https://www.zoomubik.com'));
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _checkSessionAlive();
+    }
+  }
+
+  Future<void> _checkSessionAlive() async {
+    final cookies = await CookieManager.instance().getCookies(
+      url: WebUri(_homeUrl),
+    );
+    final isLoggedIn = cookies.any(
+      (c) => c.name.startsWith('wordpress_logged_in'),
+    );
+    debugPrint(isLoggedIn ? 'Sesion activa' : 'Sin sesion');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(controller: _controller),
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri(_homeUrl),
+          ),
+          initialSettings: _settings,
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+          },
+          onLoadStop: (controller, url) async {
+            await _checkSessionAlive();
+          },
+          onReceivedError: (controller, request, error) {
+            debugPrint('Error webview');
+          },
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            final url = navigationAction.request.url?.toString() ?? '';
+            if (!url.startsWith('https://www.zoomubik.com')) {
+              return NavigationActionPolicy.CANCEL;
+            }
+            return NavigationActionPolicy.ALLOW;
+          },
+        ),
       ),
     );
   }
